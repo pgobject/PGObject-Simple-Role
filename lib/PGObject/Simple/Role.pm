39b110553d64caf3ab902ade734ea79fd3442bc4
Chris Travers <chris.travers@gmail.com>
2013-11-12 01:00:23 -0800

    Initial import

---
+++ lib/PGObject/Simple/Role.pm
@@ -0,0 +1,306 @@
+package PGObject::Simple::Role;
+
+use 5.006;
+use strict;
+use warnings;
+use Moo::Role;
+use PGObject::Simple;
+use Carp;
+
+=head1 NAME
+
+PGObject::Simple::Role - Moo/Moose mappers for minimalist PGObject framework
+
+=head1 VERSION
+
+Version 0.11
+
+=cut
+
+our $VERSION = '0.13';
+
+
+=head1 SYNOPSIS
+
+Take the following (Moose) class:
+
+    package MyAPP::Foo;
+    use Moose;
+    with 'PGObject::Simple::Role';
+
+    has id  => (is => 'ro', isa => 'Int', required => 0);
+    has foo => (is => 'ro', isa => 'Str', required => 0);
+    has bar => (is => 'ro', isa => 'Str', required => 0);
+    has baz => (is => 'ro', isa => 'Int', required => 0);
+
+    sub get_dbh {
+        return DBI->connect('dbi:Pg:dbname=foobar');
+    }
+
+And a stored procedure:  
+
+    CREATE OR REPLACE FUNCTION foo_to_int
+    (in_id int, in_foo text, in_bar text, in_baz int)
+    RETURNS INT LANGUAGE SQL AS
+    $$
+    select char_length($2) + char_length($3) + $1 * $4;
+    $$;
+
+Then the following Perl code would work to invoke it:
+
+    my $foobar = MyApp->foo(id => 3, foo => 'foo', bar => 'baz', baz => 33);
+    $foobar->call_dbmethod(funcname => 'foo_to_int');
+
+The full interface of call_dbmethod and call_procedure from PGObject::Simple are
+supported.
+
+=head1 DESCRIPTION
+
+
+
+=head1 ATTRIBUTES AND LAZY GETTERS
+
+=cut
+
+# Private attribute for database handle, not intended to be directly set.
+
+has _PGObject_DBH => ( 
+       is => 'lazy', 
+       isa => sub { 
+                    croak "Expected a database handle.  Got $_[0] instead"
+                       unless eval {$_[0]->isa('DBI::db')};
+       },
+);
+
+sub _build__PGObject_DBH {
+    my ($self) = @_;
+    return $self->_get_dbh;
+}
+
+has _PGObject_FuncPrefix => (is => 'lazy');
+
+=head1 _get_prefix
+
+Returns string, default is an empty string, used to set a prefix for mapping
+stored prcedures to an object class.
+
+=cut
+
+sub _build__PGObject_FuncPrefix {
+    return $_[0]->_get_prefix;
+}
+
+sub _get_prefix {
+    return '';
+}
+
+has _PGObject_Simple => (
+    is => 'lazy',
+);
+
+sub _build__PGObject_Simple {
+    return PGObject::Simple->new();
+}
+
+=head2 _get_dbh
+
+Subclasses or sub-roles MUST implement a function which returns a DBI database
+handle (DBD::Pg 2.0 or hgher required).  If this is not overridden an exception
+will be raised.
+
+=cut
+
+sub _get_dbh {
+    croak 'Subclasses MUST set their own get_dbh methods!';
+}
+
+=head2 call_procedure
+
+Identical interface to PGObject::Simple->call_procedure.
+
+This can be used on objects or on the packages themselves.  I.e.  
+mypackage->call_procedure() and $myobject->call_procedure() both work.
+
+=cut
+
+sub call_procedure {
+    my $self = shift @_;
+    my %args = @_;
+    $args{dbh} ||= $self->_PGObject_DBH if ref $self;
+    $args{dbh} ||= "$self"->_get_dbh;
+    my $fprefix = $self->_PGObject_FuncPrefix if ref $self;
+    $fprefix = "$self"->_get_prefix if not defined $fprefix;
+    $args{funcprefix} = $fprefix if not defined $args{funcprefix};
+    return $self->_PGObject_Simple->call_procedure(%args) if ref $self;
+    return _build__PGObject_Simple->call_procedure(%args);
+}
+
+=head2 call_dbmethod
+
+Identical interface to PGObject::Simple->call_dbmethod
+
+This can be used on objects or on the packages themselves.  I.e.  
+mypackage->call_dbmethod() and $myobject->call_dbmethod() both work.
+
+=cut
+
+sub call_dbmethod {
+    my $self = shift @_;
+    my %args = @_;
+    $args{dbh} ||= $self->_PGObject_DBH if ref $self;
+    $args{dbh} ||= "$self"->_get_dbh;
+    my $fprefix = $self->_PGObject_FuncPrefix if ref $self;
+    $fprefix = "$self"->_get_prefix if not defined $fprefix;
+    $args{funcprefix} = $fprefix if not defined $args{funcprefix};
+    if (ref $self){
+        for my $key(keys %$self){
+            $args{args}->{$key} = $self->{$key} 
+               unless defined $args{args}->{$key};
+        }
+    }
+    return $self->_PGObject_Simple->call_dbmethod(%args) if ref $self;
+    return _build__PGObject_Simple->call_dbmethod(%args);
+}
+
+#=head2 has_dbmethod (EXPERIMENTAL)
+#
+#use as __PACKAGE__->has_dbmethod (name => (default_arghash))
+#
+#For example:
+#
+#  package MyObject;
+#  use Moo;
+#  with 'PGObject::Simple::Role';
+#  has_dbmethod(save => (
+#                                 strict_args => 0,
+#                                    funcname => 'save_user', 
+#                                      schema => 'public',
+#                                        args => { admin => 0 },
+#  );
+#  $MyObject->save(args => {username => 'foo', password => 'bar'});
+#
+#Special arguments are:
+#
+#=over
+#
+#=item strict_args
+#
+#If true, args override args provided by user.
+#
+#=item returns_objects
+#
+#If true, bless returned hashrefs before returning them.
+#
+#=cut
+#
+#sub has_dbmethod {
+#    no strict 'refs';
+#    my ($target) = caller;
+#    my $name = shift;
+#    my %defaultargs = @_;
+#    my $coderef = sub {
+#       my $self = shift @_;
+#       my %args = @_;
+#       for my $key (keys %{$defaultargs{args}}){
+#           $args{args}->{$key} = $defaultargs{args}->{$key} 
+#                  unless $args{args}->{$key} or $defaultargs{strict_args};
+#       }
+#       for my $key(keys %defaultargs){
+#           next if grep(/^$key$/, qw(strict_args args returns_objects));
+#           $args{$key} = $defaultargs{$key} if $defaultargs{$key};
+#       }
+#       my @results = $self->call_dbmethod(%args);
+#       if ($defaultargs{returns_objects}){
+#           for my $ref(@results){
+#               $ref = "$target"->new(%$ref);
+#           }
+#       }
+#       return @results;
+#    };
+#    *{"${target}::${name}"} = $coderef;
+#}
+
+=head1 AUTHOR
+
+Chris Travers,, C<< <chris.travers at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-pgobject-simple-role at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=PGObject-Simple-Role>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc PGObject::Simple::Role
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=PGObject-Simple-Role>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/PGObject-Simple-Role>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/PGObject-Simple-Role>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/PGObject-Simple-Role/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 Chris Travers,.
+
+Redistribution and use in source and compiled forms with or without 
+modification, are permitted provided that the following conditions are met:
+
+=over
+
+=item 
+
+Redistributions of source code must retain the above
+copyright notice, this list of conditions and the following disclaimer as the
+first lines of this file unmodified.
+
+=item 
+
+Redistributions in compiled form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+source code, documentation, and/or other materials provided with the 
+distribution.
+
+=back
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=cut
+
+1; # End of PGObject::Simple::Role
---
+++ t/02-dbtests.t
@@ -0,0 +1,99 @@
+package test;
+
+use Moo;
+with 'PGObject::Simple::Role';
+
+has id => (is => 'ro');
+has foo => (is => 'ro');
+has bar => (is => 'ro');
+has baz => (is => 'ro');
+
+sub _get_dbh {
+    return $main::dbh;
+}
+
+package test2;
+
+use Moo;
+with 'PGObject::Simple::Role';
+
+has id => (is => 'ro');
+has foo => (is => 'ro');
+has bar => (is => 'ro');
+has baz => (is => 'ro');
+
+
+sub _get_dbh {
+    return $main::dbh;
+}
+
+sub _get_prefix {
+     return 'foo';
+};
+
+
+package main;
+use Test::More;
+use Test::Exception;
+use DBI;
+
+
+# Initial setup
+my $dbh1 = DBI->connect('dbi:Pg:', 'postgres');
+
+plan skip_all => 'Needs superuser connection for this test script' unless $dbh1;
+
+
+
+$dbh1->do('CREATE DATABASE pgobject_test_db');
+
+
+our $dbh = DBI->connect('dbi:Pg:dbname=pgobject_test_db', 'postgres');
+plan skip_all => 'No db connection' unless $dbh;
+
+plan tests => 10;
+
+$dbh->do('
+   CREATE FUNCTION public.foobar (in_foo text, in_bar text, in_baz int, in_id int)
+      RETURNS int language sql as $$
+          SELECT char_length($1) + char_length($2) + $3 * $4;
+      $$;
+') ;
+
+my ($result) = test->call_dbmethod(
+              funcname => 'foobar', 
+                  args => {id => 3, foo => 'test1', bar => 'test2', baz => 33},
+);
+is($result->{foobar}, 109, 'Correct Result, direct package call to call_dbmethod');
+my $obj = test->new(id => 3, foo => 'test1', bar => 'test2', baz => 33);
+
+($result) = $obj->call_dbmethod(funcname => 'foobar');
+is($result->{foobar}, 109, 'Correct Result, no argument overrides');
+($result) = $obj->call_procedure(funcname => 'foobar',
+                                     args => ['test1', 'testing', '3', '33']);
+is($result->{foobar}, 111, 'Correct result, call_procedure');
+($result) = test->call_procedure(funcname => 'foobar',
+                                     args => ['test1', 'testing', '3', '33']);
+is($result->{foobar}, 111, 'Correct result, direct package call to call_procedure');
+
+($result) = $obj->call_dbmethod(funcname => 'foobar', args=> {baz => 1});
+is($result->{foobar}, 13, 'Correct result, argument overrides');
+throws_ok{$obj->call_dbmethod(funcname => 'foobar', dbh => $dbh1)} qr/No such function/, 'No such function thrown using wrong db';
+
+$obj = test2->new(id => 3, foo => 'test1', bar => 'test2', baz => 33);
+
+($result) = $obj->call_dbmethod(funcname => 'bar');
+
+is($result->{foobar}, 109, 'Correct Result, no argument overrides');
+($result) = $obj->call_procedure(funcname => 'bar',
+                                     args => ['test1', 'testing', '3', '33']);
+is($result->{foobar}, 111, 'Correct result, call_procedure');
+
+($result) = $obj->call_dbmethod(funcname => 'bar', args=> {baz => 1});
+is($result->{foobar}, 13, 'Correct result, argument overrides');
+throws_ok{$obj->call_dbmethod(funcname => 'bar', dbh => $dbh1)} qr/No such function/, 'No such function thrown using wrong db';
+
+# Teardown connections
+$dbh->disconnect;
+$dbh1->do('DROP DATABASE pgobject_test_db');
+$dbh1->disconnect;
